047d328cfb9cb3bb9653b4f6b4bec2782a863256
victorious-messaging-4309 <wildayk@gmail.com>
2019-03-27 20:49:54 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,15 @@
 class Transfer
-  # your code here
+  attr_accessor :status
+  attr_reader :amount, :sender, :receiver
+
+  def initialize(amount, sender, receiver)
+    @status = "pending"
+    @sender = sender
+    @receiver = receiver
+  end
+
+  def sender
+    @sender
+  end
+
 end
